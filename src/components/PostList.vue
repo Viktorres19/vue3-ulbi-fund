60d8e56d89924a80e4a26c868e0078ae9ba4e31e
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-10-19 17:51:31 +0300

    animated actions for list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,12 +2,14 @@
   <div class="post-list">
     <template v-if="posts.length">
       <h3>Перелік користувачів:</h3>
-      <PostItem
-        :post="post"
-        v-for="post in posts"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-      />
+      <transition-group name="post-list">
+        <PostItem
+          :post="post"
+          v-for="post in posts"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+        />
+      </transition-group>
     </template>
     <template v-else>
       <h3>Ваш перелік користувачів пустий. Будь ласка, додайте першого користувача.</h3>
@@ -32,5 +34,22 @@ export default {
 <style scoped>
 .post-list {
   padding-top: 15px;
+}
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all .5s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.post-list-move {
+  transition: transform 0.5s ease;
 }
 </style>
